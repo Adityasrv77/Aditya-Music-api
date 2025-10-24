88f6b9b366ecb6d62d0cf084817600b591893f2c
Adityasrv77 <129862435+Adityasrv77@users.noreply.github.com>
2025-10-24 21:14:42 +0530

    Update endpoints.py

--- endpoints.py
+++ endpoints.py
@@ -1,5 +1,14 @@
+# endpoints.py - COMPLETE REPLACEMENT
+
+# Try multiple search endpoints
 search_base_url = "https://www.jiosaavn.com/api.php?__call=autocomplete.get&_format=json&_marker=0&cc=in&includeMetaTags=1&query="
+
 search_songs_base_url = "https://www.jiosaavn.com/api.php?__call=search.getResults&_format=json&_marker=0&cc=in&p={page}&n={limit}&q="
+
+# Alternative endpoints
+search_alt_url = "https://www.jiosaavn.com/api.php?__call=search.getResults&_format=json&_marker=0&cc=in&p={page}&n={limit}&_format=json&__src=web&q="
+
+# Other endpoints (keep as is)
 song_details_base_url = "https://www.jiosaavn.com/api.php?__call=song.getDetails&cc=in&_marker=0%3F_marker%3D0&_format=json&pids="
 album_details_base_url = "https://www.jiosaavn.com/api.php?__call=content.getAlbumDetails&_format=json&cc=in&_marker=0%3F_marker%3D0&albumid="
 playlist_details_base_url = "https://www.jiosaavn.com/api.php?__call=playlist.getDetails&_format=json&cc=in&_marker=0%3F_marker%3D0&listid="
